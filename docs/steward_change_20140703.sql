0cbb476535104dca48b2fc9893eb9f73ee73c2b3
sunnykaka <sunnykaka0721@gmail.com>
2015-03-22 21:50:57 +0800

    init

--- docs/steward_change_20140703.sql
+++
@@ -1,7 +0,0 @@
-
-/*===================================*/
-/* t_order_fetch新增字段     */
-/*===================================*/
-use steward;
-
-alter table t_order_fetch add fetch_opt_type varchar(30) default 'AUTO' comment '抓取数据操作类型：手动\自动' after fetch_data_type;
